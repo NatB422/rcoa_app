3564e0999f0e5cc8ffea6eb8320847ee4014f6cb
Nathan Brown <natb422@gmail.com>
2024-10-31 00:29:41 +0000

    Acknowledge not setup on kofi

--- about_pages/coffee.py
+++ about_pages/coffee.py
@@ -11,6 +11,8 @@ st.write("If you are finding this site useful, then great! I've made this app pu
 st.write("This is a hobby project, so it's only done in my spare time, and I'm not looking to earn anything from this.")
 st.write("If you want to contribute to me having more time to work on this site, why not use my ko-fi link to send me a tip:")
 
+st.subheader("Not currently setup")
+
 st.markdown(
     f"""
     <a href='{KOFI_URL}' target='_blank'><img height='36' style='border:0px;height:36px;'
